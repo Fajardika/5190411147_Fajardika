c7ab3479c3e2158f0114223655bae3a23c47e86c
Fajardika Dwi Yulianto <95951130+Fajardika@users.noreply.github.com>
2022-06-15 18:59:53 +0700

    5190411147

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dashboard</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" integrity="sha384-dc2NSrAXbAkjrdm9IYrX10fQq9SDG6Vjz7nQVKdKcJl3pC+k37e7qJR5MVSCS+wR" crossorigin="anonymous">
+    <style>
+        .container-fluid{ background-color: var(--bs-cyan); }
+    </style>
+  </head>
+  <body>
+<!--Header-->
+<div class="container-fluid text-center text-white">
+    <div class="row">
+        <div class="col-lg-1 py-3 d-lg-block d-sm-none d-none">
+        <img src="https://sayyidahnuraruni.files.wordpress.com/2015/04/logo-uty-png.png" class="img-fluid" alt="LOGO">
+        </div>
+        <div class="col-lg-11 col-sm-12 col-12 py-3">
+            <h2 class="fw-bolder mt-3">UNIVERSITAS TEKNOLOGI YOGYAKARTA</h2>
+            <p class="fst-italic">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque facere voluptatibus beatae.</p>
+        </div>
+    </div>
+    </div>
+    <!--Content-->
+    <div class="container-fluid text-dark bg-white">
+        <div class="row">
+            <div class="col-lg-2 col-sm-6 col-12 order-lg-first py-4">
+            <div class="list-group">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item list-group-item-action">Left sidebar</li>
+                    <li class="list-group-item list-group-item-action">A first item</li>
+                    <li class="list-group-item list-group-item-action">A second item</li>
+                    <li class="list-group-item list-group-item-action">A third item</li>
+                    <li class="list-group-item list-group-item-action">A fourth item</li>
+                </ul>
+                </div>
+            </div>
+            <div class="col-lg-8 col-sm-12 col-12 order-sm-first py-4 text-center">
+                <img src="https://4.bp.blogspot.com/-XZmvW3-Dh4g/XBcwNwh4xPI/AAAAAAAAAiU/VshuU1OGuVAG2WeJb_SOOWkQkjuHv0arwCLcBGAs/s1600/BROSUR%2BAHMAD%2BDAHLAN%2BY%2Bgel-1-2.jpg" class="img-fluid img-thumbnail" width="100%" alt="LOGO">
+            <h3>Lorem First</h3>
+            <p style="text-align: justify;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, debitis. Neque, eos id nesciunt, quidem esse quos ipsa illum voluptatem, nihil ex atque hic veritatis repellat dolorum fuga aut. Neque. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam voluptatum amet corporis atque nostrum minus accusamus, nisi eaque, nobis harum ipsa eum soluta sequi, provident nihil fuga pariatur? Ex, commodi.</p>
+            <h3>Lorem Second</h3>
+            <ul style="text-align: justify;">
+                <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat amet possimus labore officia nobis similique minus excepturi ea illum, consequatur, laudantium ipsum ipsam adipisci veniam. In quod quae architecto officia.</li>
+                <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt aperiam rem quaerat impedit doloremque quo molestias perferendis? Qui optio voluptatem tempore, natus, iure alias reprehenderit quod asperiores tempora.</li>
+                <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto impedit cupiditate obcaecati enim, mollitia veniam iusto ducimus repudiandae accusamus repellat quos nostrum dolorem. Sequi, maiores aspernatur. Excepturi beatae obcaecati iste?</li>
+            </ul>
+            </div>
+            <div class="col-lg-2 col-sm-6 py-4 d-lg-block d-sm-block d-none">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">Right Sidebar</a>
+                <a href="#" class="list-group-item list-group-item-action">A first link item</a>
+                <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+                <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+                <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!--Footer-->
+    <div class="container-fluid text-center text-white">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 col-12 py-2">
+                <h6>&copy 5190411147-Fajardika Dwi Yulianto</h6>
+            </div>
+        </div>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+//  Route::get('/', function () {
+//      return view('welcome');
+//  });
+
+// Pakai ini masih error Pak, Belum menemukan solusinya
+
+// Route::get('/register','AuthController@register');
+// Route::post('/simpanuser','AuthController@simpanuser');
+
+Route::get('/', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/simpanuser', [AuthController::class, 'simpanuser']);
+Route::post('/ceklogin', [AuthController::class, 'ceklogin']);
+Route::get('/dashboard', [AuthController::class, 'dashboard']);
+
